92b48eed199b1f2935bd677f5fe458e1b57c5f8d
seanavery <seanpollock3344@gmail.com>
2024-11-03 01:43:27 -0500

    Add turbo fingerprints

---
+++ opendbc/car/turbo/fingerprints.py
@@ -0,0 +1,25 @@
+# ruff: noqa: E501
+from opendbc.car.structs import CarParams
+from opendbc.car.turbo.values import CAR
+
+Ecu = CarParams.Ecu
+
+FINGERPRINTS = {
+  CAR.TURBO_RC_CAR: [{
+    613: 8, # test msg
+  }],
+}
+
+# debug ecu fw version is the git hash of the firmware
+FW_VERSIONS = {
+  CAR.TURBO_RC_CAR: {
+    (Ecu.engine, 0x722, None): [
+      b'0.0.01',
+      b'0.3.00a',
+      b'02/27/2022',
+    ],
+    (Ecu.debug, 0x723, None): [
+      b'put_git_hash_here',
+    ],
+  },
+}
