6aba61a638d5cc01fbd62818f414510309e4c778
seanavery <seanpollock3344@gmail.com>
2024-11-04 22:14:23 -0500

    clean car controller more

--- opendbc/car/turbo/carcontroller.py
+++ opendbc/car/turbo/carcontroller.py
@@ -1,18 +1,7 @@
 import numpy as np
 
 from opendbc.can.packer import CANPacker
-from opendbc.car import DT_CTRL
-from opendbc.car.common.pid import PIDController
-from opendbc.car.body import bodycan
-from opendbc.car.body.values import SPEED_FROM_RPM
 from opendbc.car.interfaces import CarControllerBase
-
-MAX_TORQUE = 500
-MAX_TORQUE_RATE = 50
-MAX_ANGLE_ERROR = np.radians(7)
-MAX_POS_INTEGRATOR = 0.2   # meters
-MAX_TURN_INTEGRATOR = 0.1  # meters
-
 
 class CarController(CarControllerBase):
   def __init__(self, dbc_name, CP):
