03d02eee8947818f5fcd38bc93ed8aa46aca89f6
seanavery <seanpollock3344@gmail.com>
2025-01-25 17:20:25 -0500

    Remove safety sets

--- opendbc/car/panda_runner.py
+++ opendbc/car/panda_runner.py
@@ -10,16 +10,14 @@ class PandaRunner(AbstractContextManager):
   def __enter__(self):
     self.p = Panda()
     self.p.reset()
+    self.p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
 
     # setup + fingerprinting
-    self.p.set_safety_mode(Panda.SAFETY_ELM327, 1)
     self.CI = get_car(self._can_recv, self.p.can_send_many, self.p.set_obd, True)
     assert self.CI.CP.carFingerprint.lower() != "mock", "Unable to identify car. Check connections and ensure car is supported."
 
     safety_model = CarParams.SafetyModel.schema.enumerants[self.CI.CP.safetyConfigs[0].safetyModel]
-    self.p.set_safety_mode(Panda.SAFETY_ELM327, 1)
     self.CI.init(self.CI.CP, self._can_recv, self.p.can_send_many)
-    self.p.set_safety_mode(safety_model, self.CI.CP.safetyConfigs[0].safetyParam)
 
     return self
 
