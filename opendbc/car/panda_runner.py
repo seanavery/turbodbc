9246cba357dd3cf7678d3d3c7f19af21abca2b28
seanavery <seanpollock3344@gmail.com>
2025-04-15 05:41:42 -0400

    Lint

--- opendbc/car/panda_runner.py
+++ opendbc/car/panda_runner.py
@@ -16,11 +16,11 @@ class PandaRunner(AbstractContextManager):
     self.CI = get_car(self._can_recv, self.p.can_send_many, self.p.set_obd, True)
     assert self.CI.CP.carFingerprint.lower() != "mock", "Unable to identify car. Check connections and ensure car is supported."
 
-    safety_model = self.CI.CP.safetyConfigs[0].safetyModel
     self.p.set_safety_mode(CarParams.SafetyModel.elm327, 1)
     self.CI.init(self.CI.CP, self._can_recv, self.p.can_send_many)
     # hardcoding to turbo safety for now
     # TODO(simcity): unwrap enum value from safetyConfigs
+    # safety_model = self.CI.CP.safetyConfigs[0].safetyModel
     self.p.set_safety_mode(CarParams.SafetyModel.turbo, self.CI.CP.safetyConfigs[0].safetyParam)
 
 
